61bbd5a7b3a157f967ae0f95c38b845ffd5b34f5
Boipelo Zuma <pelothedeveloper@gmail.com>
2024-11-14 07:04:56 +0200

    feat: add properties tovplayer state and prepare for player control

--- platform-runner/src/platform_runner/controls.clj
+++ platform-runner/src/platform_runner/controls.clj
@@ -1,5 +1,15 @@
 (ns platform-runner.controls)
 
 (defn control-player [player key-code]
+  (condp = key-code
+    java.awt.event.KeyEvent/VK_LEFT "Left"
+    java.awt.event.KeyEvent/VK_RIGHT "Right"
+    java.awt.event.KeyEvent/VK_UP "Up"
+    java.awt.event.KeyEvent/VK_DOWN "Down"
+    java.awt.event.KeyEvent/VK_SPACE "Space"
+    java.awt.event.KeyEvent/VK_ENTER "Enter"
+    java.awt.event.KeyEvent/VK_BACK_SPACE "Back"
+    java.awt.event.KeyEvent/VK_TAB "Tab"
+    player)
   ;; TODO...
   )
--- platform-runner/src/platform_runner/state.clj
+++ platform-runner/src/platform_runner/state.clj
@@ -1,5 +1,16 @@
 (ns platform-runner.state
   (:require [platform-runner.config :refer [config]]))
 
+
+
 (def player (atom {:x (+ 20 (apply max (:x-range (:viewport config))))
-                         :y (+ 20 (apply min (:y-range (:viewport config))))}))
+                   :y (+ 20 (apply min (:y-range (:viewport config))))
+                   :moving-x 0
+                   :moving-y 0
+                   :crouching false
+                   :poised-to-jump false
+                   :falling false
+                   :health 100
+                   :damage-frames 0 ;; how many frames to stun, couting down
+                   :damage-per-frame 0
+                   }))
